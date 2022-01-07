68001877ee43b3e31e5188b23836e6bb1973864a
arashi87 <arasi27676271@gmail.com>
2022-01-08 05:55:29 +0800

    Feat: implement whole memory manager system

--- MemManager.c
+++ MemManager.c
@@ -1,3 +1,8 @@
+#include "manager/disk.c"
+#include "manager/page.c"
+#include "manager/physical.c"
+#include "manager/tlb.c"
+#include "manager/victim.c"
 #include "module/utils.c"
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,5 +15,92 @@ char tlb_policy[10], page_policy[10], frame_policy[10];
 
 int main()
 {
+    // parse trace
     pasrse_config(tlb_policy, page_policy, frame_policy, &process_num, &virtual_num, &physical_num);
+
+    char proc, p_proc;                     // process, prev process
+    int pfn, vpn, number_o, number_i;      // physical frame, virtual page, disk block number out/in
+    Disk *disk = Dinit();                  // disk use to save page out vpn
+    TLBUffer *TLB = TLBInit();             // TLB
+    PTable *PT = PTInit(virtual_num);      // page table
+    PMemory *phys = PMinit(physical_num);  // physical mnemory
+    Queue *victim = QInit();               // victim page queue
+    FILE *trace = fopen("trace.txt", "r"); // trace file
+
+    while (~fscanf(trace, "Reference(%c, %d)\n", &proc, &vpn))
+    {
+        // fluch TLB if process change
+        if (proc != p_proc)
+            TLB = TLBInit();
+        printf("Process %c, ", proc);
+
+        // memory manager
+        if (~(pfn = TLBHit(TLB, vpn))) // TLB hit
+            printf("TLB Hit, %d=>%d\n", vpn, pfn);
+        else // TLB miss
+        {
+            printf("TLB Miss, ");
+            if (~(pfn = PTHit(PT, proc - 'A', vpn))) // page hit
+            {
+                printf("Page Hit, %d=>%d\n", vpn, pfn);
+            }
+            else // page fault
+            {
+                printf("Page Fault, ");
+                if (PTIsInDisk(PT, proc - 'A', vpn))
+                {
+                    // vpn is in disk, need to move it back to physical memory
+                    QNode *node = VPReplace(victim, proc - 'A', vpn, page_policy[0] == 'F',
+                                            frame_policy[0] == 'G'); // node which need to kick to disk
+                    pfn = PTGetPFN(PT, node->proc, node->vpn);
+                    number_i = DPageIn(disk, node->proc, node->vpn);
+                    number_o = DPageOut(disk, proc - 'A', vpn);
+                    printf("%d, Evict %d of Process %c to %d, %d<<%d\n", pfn, node->vpn, node->proc + 'A', number_i,
+                           vpn, number_o);
+
+                    // clear kicked node's TLB and PT
+                    TLBClear(TLB, node->vpn, pfn);
+                    PTUpdate(PT, node->proc, node->vpn, 0, 0);
+
+                    // update asked vpn
+                    PTUpdate(PT, proc - 'A', vpn, pfn, 1);
+                    if (page_policy[0] != 'C')
+                        VPInsert(victim, proc - 'A', vpn);
+                }
+                else
+                {
+                    if (~(pfn = PMFindFree(phys))) // free space found
+                    {
+                        printf("%d, Evict -1 of Process %c to -1, %d<<-1\n", pfn, proc, vpn);
+                        PMInsert(phys, pfn); // insert vpn into physical memory (only need to mark pfn used)
+                        VPInsert(victim, proc - 'A', vpn);     // insert vpn into victim queue
+                        PTUpdate(PT, proc - 'A', vpn, pfn, 1); // update page table vpn with pfn
+                    }
+                    else // no free space, try to kick page in victim queue
+                    {
+                        QNode *node = VPReplace(victim, proc - 'A', vpn, page_policy[0] == 'F',
+                                                frame_policy[0] == 'G'); // node which need to kick to disk
+                        pfn = PTGetPFN(PT, node->proc, node->vpn);
+                        number_i = DPageIn(disk, node->proc, node->vpn);
+                        printf("%d, Evict %d of Process %c to %d, %d<<-1\n", pfn, node->vpn, node->proc + 'A', number_i,
+                               vpn);
+
+                        // clear kicked node's TLB and PT
+                        TLBClear(TLB, node->vpn, pfn);
+                        PTUpdate(PT, node->proc, node->vpn, 0, 0);
+
+                        // update asked vpn
+                        PTUpdate(PT, proc - 'A', vpn, pfn, 1);
+                        if (page_policy[0] != 'C')
+                            VPInsert(victim, proc - 'A', vpn);
+                    }
+                }
+            }
+            TLBInsert(TLB, vpn, pfn, (tlb_policy[0] == 'L'));
+            TLBHit(TLB, vpn);
+            printf("Process %c, TLB Hit, %d=>%d\n", proc, vpn, pfn);
+        }
+        QClockUpdate(victim, proc - 'A', vpn);
+        p_proc = proc;
+    }
 }
