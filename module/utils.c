aef670a6897eaaf9512eac0bd63cc8ce92ce236f
arashi87 <arasi27676271@gmail.com>
2022-01-09 03:43:12 +0800

    Feat: add result struct and function to get EAT & Page fault rate

--- module/utils.c
+++ module/utils.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+typedef struct
+{
+    float TLB_hit_success;
+    float TLB_hit_amount;
+    float Page_hit_fault;
+    float Page_hit_amount;
+} Result;
+
+float get_eat(Result *result, int proc)
+{
+
+    if (result[proc].TLB_hit_amount)
+    {
+        float alpha = result[proc].TLB_hit_success / result[proc].TLB_hit_amount;
+        return (float)(120 * alpha + 220 * (1 - alpha));
+    }
+    return 0;
+}
+
+float get_page_fault_rate(Result *result, int proc)
+{
+    if (result[proc].Page_hit_amount)
+        return result[proc].Page_hit_fault / result[proc].Page_hit_amount;
+    return 0;
+}
+
 void pasrse_config(char *tlb, char *page, char *frame, int *ps_num, int *vl_num, int *pl_num)
 {
     FILE *config = fopen("sys_config.txt", "r");
